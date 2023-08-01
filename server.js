fa26f8fca693446219834627158d40b5f25c2e47
ramiyengar <ramanujank@gmail.com>
2023-08-01 19:54:08 +0530

    adding node js files to sample repo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Powered By Paketo Buildpacks</title>
+  </head>
+  <body>
+    <h1>Hello KubeCon demo</h1>
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
+  </body>
+</html>`);
+});
+
+app.listen(port);
